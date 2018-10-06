639a0385cc4ac2db751bf2bf9f6c495416265203
Matt Eubanks <spc.eubanks.matthew@gmail.com>
2018-10-06 15:35:57 -0700

    cors

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ app.use(express.static('client/build'));
 
 // app.use(cors());
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://fast-forest-94723.herokuapp.com/'
+  );
   res.header('Access-Control-Allow-Credentials', true);
   res.header(
     'Access-Control-Allow-Headers',
