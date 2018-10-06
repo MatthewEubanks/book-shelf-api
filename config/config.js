acb109bfe79d97804af6032932e1be9bb2dd6322
Matt Eubanks <spc.eubanks.matthew@gmail.com>
2018-10-06 10:42:21 -0700

    adding cors

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const config = {
   },
   default: {
     SECRET: 'SUPERSECRETPASSWORD123',
-    DATABASE: 'mongodb://localhost:27017/booksShelf',
+    DATABASE: 'mongodb://user1:password1@ds119343.mlab.com:19343/book_shelf',
   },
 };
 
@@ -14,7 +14,8 @@ exports.get = function get(env) {
 };
 
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || 'mongodb://localhost:27017/books_Shelf';
+  process.env.DATABASE_URL ||
+  'mongodb://user1:password1@ds119343.mlab.com:19343/book_shelf';
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || 'mongodb://localhost/book_shelf';
 exports.PORT = process.env.PORT || 3001;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const config = require('./config/config').get(process.env.NODE_ENV);
-// const cors = require('cors');
+const cors = require('cors');
 const app = express();
 
 mongoose.Promise = global.Promise;
@@ -18,15 +18,15 @@ app.use(cookieParser());
 
 app.use(express.static('client/build'));
 
-// app.use(cors());
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+app.use(cors());
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   next();
+// });
 
 // GET //
 app.get('/api/auth', auth, (req, res) => {
