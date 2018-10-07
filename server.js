06e035ed2b837c09cce7c09c9cc13ce46bb2aeea
Matt Eubanks <spc.eubanks.matthew@gmail.com>
2018-10-06 18:58:07 -0700

    change cors

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.use(express.static('client/build'));
 // app.use(cors());
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
