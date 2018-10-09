5b1f3c7ab314101ce71f37a3171bc62232b59bdb
Matt Eubanks <spc.eubanks.matthew@gmail.com>
2018-10-08 21:54:16 -0700

    reverting to working branch

--- config/config.js
+++ config/config.js
@@ -19,7 +19,7 @@ exports.DATABASE_URL =
   process.env.DATABASE_URL ||
   'mongodb://user1:password1@ds119343.mlab.com:19343/book_shelf';
 exports.TEST_DATABASE_URL =
-  process.env.TEST_DATABASE_URL || 'mongodb://localhost/book_shelf';
+  process.env.TEST_DATABASE_URL || 'mongodb://localhost/test_book_shelf';
 exports.PORT = process.env.PORT || 3001;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'Liam';
 // exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '5d';
